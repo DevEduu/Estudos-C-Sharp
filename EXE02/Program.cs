68a4091b6662d05d11afde1957d67bc64b33b2e6
EduPerto <eduapxnet@gmail.com>
2024-04-24 09:34:49 -0300

    Adição de novos exercircios

---
+++ EXE02/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Me informe seu nome:");
+string name = Console.ReadLine();
+Console.WriteLine("Sejá bem vindo " + name);
---
+++ EXE03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Crie a soma de dois números e mostre a soma entre eles
+Console.WriteLine("Script de soma de dois números");
+Console.WriteLine("Informa o primeiro número:");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe a segundo número: ");
+int n2 = int.Parse(Console.ReadLine());
+int soma = n1 + n2;
+Console.Write("A soma entre " + n1+"+"+n2+"="+soma);
+

a34cf6cf134f8725b23a3c167c24e4d8faedcc6c
EduPerto <eduapxnet@gmail.com>
2024-04-24 11:01:42 -0300

    projetos 04

---
+++ EXE04/Program.cs
@@ -0,0 +1,5 @@
+﻿ // Script para identificar qual tipo primitivo ele é 
+
+Console.WriteLine("Digite alguma coisa! ");
+objecto = 
+Console.WriteLine("O que que você digitou é do tipo: " + objecto);
